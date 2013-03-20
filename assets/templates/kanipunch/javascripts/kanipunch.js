10a2966c554a865bd8cf3e344715e01314bfe029
Tatsuya Ishiguro <gurorin@gmail.com>
2013-03-21 02:58:48 +0900

    ver 0.7

--- assets/templates/kanipunch/javascripts/kanipunch.js
+++ assets/templates/kanipunch/javascripts/kanipunch.js
@@ -51,17 +51,103 @@ jQuery(document).ready(function() {
 	//ページトップへ
 	anchorWithinAPage(".pagetop a");
 	
-	//サイドメニュー制御
+	
+	
+	/* ------------------------------------
+	
+	   サイドメニュー制御
+	
+	--------------------------------------- */
+	
+	// 「#menu」が存在するときのみ動作する
+	if($('.sidemenu').length){
+	
+	var active = '';
+	
+	// 現在のURLを配列で取得
+	var nowURL = document.URL.split('/');//var nowURL = location.href.split('/');
+	
+	// 親のディレクトリ取得
+	var endDir = nowURL.slice(nowURL.length-2,nowURL.length-1) + active;
+	
+	
+	
+	/* 現在のカテゴリのサブメニューをopen
+	--------------------------------------- */
+	
+	// ディレクトリ名によって分岐
+	switch (endDir){ 
+	case 'company':
+	var active = 2;
+	break;
+	case 'service':
+	var active = 3;
+	break;
+	case 'product':
+	var active = 4;
+	break;
+	case 'inquiry':
+	var active = 5;
+	break;
+	}
+	
+	// Class を追加する対象を決定
+	var targetElem = '.sidemenu > ul > li:nth-child(' + active + ') > ul';
+	
+	// 対象へClassを追加
+	//$(targetElem).addClass('open');
+	
+	// 対象のCSSを指定
+	$(targetElem).css('display','block');
+	
+	}
+	
+	
+	
+	/* 現在のページをハイライト
+	--------------------------------------- */
+	var num = nowURL.length - 2;//現在のURLから階層を判別
+
+	for (var i = 0; i < num; i++){
+		nowURL.shift();//末尾の2つのみに配列を整形
+	}
+
+  var currentLink;
+  while(nowURL.length){
+    currentLink=$('.sidemenu > ul > li > ul > li').find("a[href$='../"+nowURL.join('/')+"']");
+    if(currentLink.length){
+      currentLink.css({
+        //fontWeight: 'bold',
+				backgroundColor: '#F0F0F0'
+        //color: '#73C71F'
+      });
+      break;
+    }
+    nowURL.pop();
+  }
+	
+	
+	
+	/* サブメニューのtoggle機能
+	--------------------------------------- */
+	
+	//ルートまでの相対パスを取得
+	var rootPath = "";
+	var repeatNum = num - 3;
+	
+	for (var i = 0; i < repeatNum; i++){
+		var rootPath = rootPath + "../";
+	}
+	
 	$(".sidemenu ul li a").click(function(){
 		$(this).next("ul").slideToggle().toggleClass("open");
 		//カテゴリTOPも遷移可能にする
-		/*
 		if($(this).next("ul").hasClass('open')) {
 			$(".sidemenu ul li:has(ul) > a[href]").unbind("click", handler1);
+			$(".sidemenu ul li:has(ul) > a").css('background-image','url(' + rootPath + 'assets/templates/kanipunch/images/arrow.png)');
 		} else {
 			$(".sidemenu ul li:has(ul) > a[href]").bind("click", handler1);
     }
-		*/
 	});
 	$(".sidemenu ul li:has(ul) > a[href]").bind("click", handler1);
 });
@@ -73,7 +159,7 @@ function handler1(e){
 
 
 /* ページトップへ
---------------------------------------*/
+----------------------------------------------------------*/
 function anchorWithinAPage(node) {
     jQuery(node).click(function(e) {
         e.preventDefault();
